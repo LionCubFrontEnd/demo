6b6f7328836742aa2f3d3b31a0d4e031d0d92e47
Chocolate1999 <2816778271@qq.com>
2022-08-31 21:22:26 +0800

    feat: add remote app

--- module-federation/remote-demo/src/App.js
+++ module-federation/remote-demo/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div
+        style={{
+          margin: "24px",
+          padding: "24px",
+          textAlign: "center",
+          backgroundColor: "#fefe",
+        }}
+      >
+        <h1>this is remote-demo</h1>
+      </div>
     </div>
   );
 }
